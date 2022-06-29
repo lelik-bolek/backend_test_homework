58c997f54c8e9558e5474a250260d04441ab450a
Леликов Александр <lelikovaa@mail.ru>
2022-06-29 10:57:56 +0300

    Добавлены линтеры. Создан файл с  Правилами оформения

---
+++ code_rule.py
@@ -0,0 +1,26 @@
+from math import sqrt
+
+message = ('Добро пожаловать в самую'
+           'лучшую программу для вычисления'
+           'квадратного корня из заданного числа')
+
+
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number):
+    """Показывает результат вычисления квадратного корня."""
+    if your_number <= 0:
+        return
+    else:
+        root = calculate_square_root(your_number)
+        print(
+            f'Мы вычислили корень квадратный из введенного вами числа.'
+            f' Это будет: {root}'
+            )
+
+
+print(message)
+calc(25.5)
